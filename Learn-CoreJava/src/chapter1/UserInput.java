d339500a7587543370e83a8300cae5a3568a18ef
Shailendra12 <Shailendra12@shailendra123>
2019-12-18 22:25:16 +0530

    UserInput

--- Learn-CoreJava/src/chapter1/UserInput.java
+++ Learn-CoreJava/src/chapter1/UserInput.java
@@ -8,11 +8,18 @@ public class UserInput {
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		
+		// Integer Value
 		System.out.println("Enter Any Digit");
-		
 		int UserInput = scan.nextInt();
-		
 		System.out.println(UserInput);
+		
+		
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("Enter Your name");
+		String yourname = scan1.nextLine();
+		System.out.println(yourname);
+		
+		
 		
 		
 		
