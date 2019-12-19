a4f26a98881c3f74c5c62563c071f2dfb521cc08
Shailendra12 <Shailendra12@shailendra123>
2019-12-19 22:08:30 +0530

    19 dec 10:08 PM

---
+++ Learn-CoreJava/src/chapter1/Increament_Operator.java
@@ -0,0 +1,19 @@
+package chapter1;
+
+public class Increament_Operator {
+	
+	public static void main(String[] args) {
+		
+		//  i++ means post increment of i. Here ++ refers to increment by 1 .
+		int i =10;
+		
+		i+=5;
+		
+		System.out.println(i);
+		
+		
+	
+		
+	}
+
+}
